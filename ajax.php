cfb9bc35b9ffe941a33058e6828074cee90d3820
martin-mueller <martin_mu@gmx.de>
2013-04-15 16:12:38 +0200

    FirstCommit

---
+++ ajax.php
@@ -0,0 +1,39 @@
+<?php 
+$db = new PDO('sqlite:zettel.sqlite');
+$db->exec('CREATE TABLE IF NOT EXISTS zettel (id, key, value, PRIMARY KEY (id, key))');
+
+
+if ($_GET['action']){
+	$action = $_GET['action'];
+	if ($action == 'getAll') {
+		$res = $db->query("SELECT * FROM zettel");
+		if ($res){
+			while($row = $res->fetch(PDO::FETCH_ASSOC)){
+					$data[] = $row;
+				}
+				echo json_encode($data);
+			}
+		else echo 'null';	
+	}
+	elseif ($action == 'clearAll') {
+
+		echo $db->exec('DELETE FROM zettel');
+	}
+}
+
+extract($_POST);
+
+if ($id){
+	if ($pos)  save($id, 'pos',  $pos);
+	if ($size) save($id, 'size', $size);
+	if ($text!==null) save($id, 'text', $text);
+}
+
+function save($id,$key,$value){
+	global $db;
+	if (is_array($value)) $value=implode(',',$value);
+	echo "Saving $id,$key";
+	$sql = "INSERT OR REPLACE INTO zettel (id,key,value) VALUES ('$id','$key','$value') ";
+	$res = $db->exec($sql);
+	// var_dump($res);
+}
